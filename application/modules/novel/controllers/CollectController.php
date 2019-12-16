4facc6766d3cdfcf4c9e2551578d9d472a1cda1c
叶林森 <yelinsen@jiayouhui360.com>
2019-12-16 10:11:29 +0800

    test

--- application/modules/novel/controllers/CollectController.php
+++ application/modules/novel/controllers/CollectController.php
@@ -389,7 +389,7 @@ class CollectController extends BaseController
                     return $this->ajaxReturn(0, $res['msg'] . ' 采集完成');
                 }
 
-                return $this->ajaxReturn(0, $res['msg'], null, ['url' => '/novel/collect/doArticle.html?act=' . $act . '&book_id=' . $bookId . '&collect_id=' . $collectId . '&chapter_id=' . $chapterId . '&category_id=' . $categoryId . '&from_sort=' . $res['from_sort']]);
+                return $this->ajaxReturn(0, $res['msg'], null, ['url' => '/novel/collect/doArticle.html?act=' . $act . '&book_id=' . $bookId . '&collect_id=' . $collectId . '&chapter_id=' . $chapterId . '&category_id=' . $categoryId . '&from_sort=' . $res['from_sort'], 'test' => $res]);
             } catch (ManageException $e) {
                 return $this->ajaxReturn(1, $e->getMessage());
             } catch (Exception $e) {
